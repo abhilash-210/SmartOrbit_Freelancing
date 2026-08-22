6837c1d768e05fa07305afeb330a2e2a927fa6e1
abhilash-gp210 <abilashg745@gmail.com>
2026-08-23 00:42:45 +0530

    feat: add pre-filled text to WhatsApp links

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,5 +1,5 @@
 export const socialLinks = {
-  whatsapp: "https://wa.me/918019677679",
+  whatsapp: "https://wa.me/918019677679?text=Hello%20SmartOrbit%20Freelancers%2C%20I%20am%20interested%20in%20your%20freelancing%20services.%20I%20would%20like%20to%20discuss%20about%20project.",
   email: "mailto:freelancingxaitech@gmail.com",
   phone: "tel:+918019677679",
   github: "", // ADD GITHUB URL HERE
